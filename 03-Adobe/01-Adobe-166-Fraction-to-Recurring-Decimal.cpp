ac846719cd906d51b50bad897da04412afec9bdb
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-01-11 02:13:35 +0530

    Create Folder 03-Adobe

---
+++ 03-Adobe/01-Adobe-166-Fraction-to-Recurring-Decimal.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                 long long
+// #define int                long long
+#define mod                1000000007
+#define inf                1e18
+const ll N = 1e5 + 5;
+void fastIO() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+
+
+//class Solution {
+//
+//public:
+//
+//};
+
+
+
+int32_t main() {
+    fastIO();
+    // Solution sol;
+    
+    
+    
+    return 0;
+}
+    
+
+
+
