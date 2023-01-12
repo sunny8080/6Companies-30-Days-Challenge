7b02ebe34696c82105ed1fa515815f3528a91204
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-01-13 05:23:47 +0530

    Add Adb Q1

--- 03-Adobe/01-Adobe-166-Fraction-to-Recurring-Decimal.cpp
+++ 03-Adobe/01-Adobe-166-Fraction-to-Recurring-Decimal.cpp
@@ -12,23 +12,49 @@ void fastIO() {
 
 
 
-//class Solution {
-//
-//public:
-//
-//};
+// Link:- https://leetcode.com/problems/fraction-to-recurring-decimal/description/
 
+class Solution {
+public:
+    string fractionToDecimal(int ni, int di) {
+        string ans, frac;
+        // if ((ni < 0 || di < 0) && (ni > 0 || di > 0)) ans += "-";
+        if (ni != 0 && (ni > 0 ^ di > 0)) ans += "-";
+
+        long n = labs(ni);
+        long d = labs(di);
+        ans += to_string(n / d);
+        n %= d;
+        if (n == 0) return ans;
+
+        unordered_map<int, int> mpp;
+
+        for (int i = 1;i <= (int)1e4 && n; i++) {
+            if (mpp[n]) {
+                frac.insert(mpp[n] - 1, "(");
+                frac += ")";
+                break;
+            }
+            mpp[n] = i;
+            n *= 10;
+            frac += to_string(n / d);
+            n %= d;
+        }
+
+        return ans + "." + frac;
+    }
+};
 
 
 int32_t main() {
     fastIO();
     // Solution sol;
-    
-    
-    
+
+
+
     return 0;
 }
-    
+
 
 
 
